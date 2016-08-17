208ce4b33122798baaec0d812035aeca5fadb676
lance <li6151770@126.com>
2016-08-17 10:08:38 +0800

    add config constants

---
+++ src/main/java/com/lance/code/generation/common/ConfigConstants.java
@@ -0,0 +1,36 @@
+package com.lance.code.generation.common;
+
+public interface ConfigConstants {
+	/**定义Schema*/
+	String SCHEMA = "longchou-loan";
+	
+	/**移除表前缀*/
+	String REMOVE_TABLE_PREFIX = "t_";
+	
+	/**Domain后缀, CustomerInfo*/
+	String DOMAIN_SUFFIX = "";
+	
+	/**生成文件路径*/
+	String FILE_PATH = "E:\\gitwork\\code-generation\\src\\main\\java";
+	
+	/**MyBatis SQL生成文件路径*/
+	String SQL_PATH = "E:\\gitwork\\code-generation\\src\\main\\resources\\mappers";
+	
+	/**生成Mapper.xml后缀名字, EX: customer-mapper.xml*/
+	String SQL_MAPPER_SUFFIX = "-mapper";
+	
+	/**生成包名称*/
+	String ROOT_PACKAGE = "com.lance.code";
+	
+	/**JavaBean包名称*/
+	String DOMAIN_PACKAGE = "domain";
+	
+	/**service包名称*/
+	String SERVICE_PACKAGE = "service";
+	
+	/**serviceImpl包名称*/
+	String SERVICE_impl_PACKAGE = "serviceImpl";
+	
+	/**mapper包名称*/
+	String MAPPER_PACKAGE = "mapper";
+}
