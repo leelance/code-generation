f413620a085dbdcc00876a67a47ac4e55df8b4c4
lance <li6151770@126.com>
2016-08-17 10:09:18 +0800

    add define keywords

---
+++ src/main/java/com/lance/code/generation/common/KeyWords.java
@@ -0,0 +1,60 @@
+package com.lance.code.generation.common;
+
+public interface KeyWords {
+	/**package关键字*/
+	String PACKAGE = "package";
+	
+	/**interface关键字*/
+	String INTERFACE = "interface";
+	
+	/**class关键字*/
+	String CLASS = "class";
+	
+	/**public关键字*/
+	String PUBLIC = "public";
+	
+	/**private关键字*/
+	String PRIVATE = "private";
+	
+	/**implements关键字*/
+	String IMPLEMENTS = "implements";
+	
+	/**Serializable关键字*/
+	String Serial = "Serializable";
+	
+	/**import关键字*/
+	String IMPORT = "import";
+	
+	/**get关键字*/
+	String GET = "get";
+	
+	/**set关键字*/
+	String SET = "set";
+	
+	/**return关键字*/
+	String RETURN = "return ";
+	
+	/**void关键字*/
+	String VOID = "void ";
+	
+	/**this关键字*/
+	String THIS = "this";
+	
+	/**=关键字*/
+	String EQUAL = " = ";
+	
+	/**空格*/
+	String SPACE = " ";
+	
+	/**点号*/
+	String DOT = ".";
+	
+	/**分号*/
+	String SEMICOLON = ";";
+	
+	/**换行*/
+	String NEWLINE = "\r\n";
+	
+	/**Tab符号*/
+	String Tab = "\t";
+}
